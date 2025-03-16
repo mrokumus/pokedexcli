d9b5497ce759a9b57e364cdd503efe787c75f768
M.Rasit <hi@okumus.dev>
2025-03-16 07:23:31 +0300

    feat: capture the cmd

--- main.go
+++ main.go
@@ -1,12 +1,34 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"strings"
 )
 
 func main() {
-	fmt.Print("Hello, World!\n")
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex >")
+		scanner.Scan()
+		words := strings.Fields(strings.ToLower(strings.TrimSpace(scanner.Text())))
+		if len(words) == 0 {
+			help()
+			continue
+		}
+		switch words[0] {
+		case "help":
+			help()
+			break
+		case "exit":
+			exit()
+			os.Exit(0)
+		default:
+			use(words[0])
+			break
+		}
+	}
 }
 
 func cleanInput(text string) []string {
@@ -16,4 +38,16 @@ func cleanInput(text string) []string {
 	var words []string
 	words = strings.Split(strings.TrimSpace(text), " ")
 	return words
+}
+
+func exit() {
+	fmt.Printf("Closing the Pokedex... Goodbye!")
+}
+
+func help() {
+	fmt.Println("Welcome to the Pokedex!\nUsage:\n\nhelp: Displays a help message\nexit: Exit the Pokedex")
+}
+
+func use(cmd string) {
+	fmt.Println(cmd)
 }
